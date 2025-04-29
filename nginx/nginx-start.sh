47feeed14b67feda3106a94a418165839ab9ad88
Jonathan Rooke <jdoggus@gmail.com>
2025-04-29 08:50:06 -0400

    added TRUSTED_LAN to .envs and modded nginx conf files and sh to parameterize

--- nginx/nginx-start.sh
+++ nginx/nginx-start.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# Set NGINX_ENV=dev or NGINX_ENV=prod in docker-compose.yml
+
+# Pick correct nginx.conf based on environment
+if [ "$NGINX_ENV" = "dev" ]; then
+    TEMPLATE_CONF=/etc/nginx/nginx.dev.conf
+else
+    TEMPLATE_CONF=/etc/nginx/nginx.prod.conf
+fi
+
+# Substitute environment variables and start nginx
+envsubst '$TRUSTED_LAN' < $TEMPLATE_CONF > /etc/nginx/nginx.conf
+nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
+
+
 echo "🔁 Waiting for wordpress to resolve..."
 
 until getent hosts wordpress > /dev/null; do
